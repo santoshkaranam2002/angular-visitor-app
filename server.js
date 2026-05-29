6bedba5265afd4c93aff793163fb387e108f8f02
SowndaryaVajrala <chandana192001@gmail.com>
2026-05-29 11:10:48 +0530

    server.js chnaged

--- server.js
+++ server.js
@@ -8,5 +8,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/visitor-hub/browser/index.html'));
 });
 
-const PORT = process.env.PORT || 8080;
+// iisnode passes a named pipe via PORT environment variable
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
